8b36b3581c348f08ee69a59c18f1f9f004109621
Kosei-16 <ciku22126@g.nihon-u.ac.jp>
2024-12-03 10:24:59 +0900

    Add codes directory

--- codes/com/craftinginterpreters/lox/Lox.java
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+//import java.util.List;
 
 public class Lox {
   static boolean hadError = false;
@@ -45,7 +45,7 @@ public class Lox {
     //List<Token> tokens = scanner.scanTokens();
 
     //for(Token token : tokens) {
-      //System.out.println(token);
+    //  System.out.println(token);
     //}
   }
 
